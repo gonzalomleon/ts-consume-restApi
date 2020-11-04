401c84c60ffac75ed8943b1f5199c14e730572cf
Gonzalo León <gonzalomleon@gmail.com>
2020-11-04 20:43:58 -0300

    Practice changes

--- angularApp/my-app/src/app/api-req.service.ts
+++ angularApp/my-app/src/app/api-req.service.ts
@@ -1,14 +1,26 @@
 import { Item } from './item';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export class MenuItem {
+  id: number;
+  usuario: string;
+  menuitems: string[];
+}
 
 @Injectable({
   providedIn: 'root',
 })
 export class ApiReqService {
+
   apiURL = 'http://localhost:3002/api';
   constructor(private httpClient: HttpClient) {}
   public getItems() {
     return this.httpClient.get<Item[]>(`${this.apiURL}/items`);
+  }
+
+  public login(): Observable<MenuItem> {
+    return this.httpClient.get<MenuItem>(`${this.apiURL}/login`);
   }
 }
--- angularApp/my-app/src/app/app.component.ts
+++ angularApp/my-app/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
-import { ApiReqService } from './api-req.service';
+import { ApiReqService, MenuItem } from './api-req.service';
 import { Item } from './item';
 
 import { DatatableComponent } from './compo/datatable.component';
@@ -14,19 +14,27 @@ import { TableTemplate } from './compo/table-template';
 })
 export class AppComponent implements OnInit {
   title = 'my-app';
+  menuItem: MenuItem;
   constructor(private apiService: ApiReqService) {}
   itemsData: Item[];
+  nothingToShow:string = "Nothing to show";
 
   ngOnInit() {
     console.log('Initialize AppComponent');
-    this.apiService.getItems().subscribe((res) => {
-      this.itemsData = res;
-      console.log(...res);
-      });
+    // this.apiService.getItems().subscribe((res) => {
+    //   this.itemsData = res;
+    //   console.log(...res);
+    // });
   }
 
-  agregar(){
-    
-    alert("Hola");
-   }
+  agregar() {
+    alert('Hola');
+  }
+  login(){
+    this.nothingToShow = "Loading...";
+    this.apiService.login().subscribe((res) => {
+      this.menuItem = res;
+    });
+    this.nothingToShow = "Finished loading...";
+  }
 }
